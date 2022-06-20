ced3058f82c429b92e590e898a989ab7fb8e1065
Padz <benpadz08@gmail.com>
2022-06-20 13:31:04 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
         'mysql_hris' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('HRIS_DB_HOST'),
+            'host' => 'localhost',
             'port' => '3306',
             'database' => 'hris',
             'username' => 'root',
